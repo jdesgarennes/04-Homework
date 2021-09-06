7441ba17d71b7ba89d4b98caa518e9896504ec30
jdesgarennes <johndesgarennes@gmail.com>
2021-09-06 16:23:43 -0700

    Created successfull loop and choice of awnsers

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 
 var questionsConainer = document.querySelector("#questions-box");
 var BeginGameButton = document.querySelector("#start");
-
+var score =0
 
 // Creste a array of objexts that contain all possible questions with awnser Keys.
 var quizquestions = [
     {
         question: "Question ONE: What Does HTML stand for?",
         answers: ["A: How To MarkUp Language","B: Hypertext MarkUP Language", "C: Hold languge", "D: There is no meaning"],
-        correctAnswer: "A: How To MarkUp Language",
+        correctAnswer: "B: Hypertext MarkUP Language",
     },
     {
         question: "Question TWO: What is javaScript?",
@@ -31,7 +31,7 @@ var quizquestions = [
 var curentQuestion =0;
 
 function beginGame () {
-// next line clear out text box.    
+// next line clears out text box.    
 questionsConainer.innerHTML = "";
 var questions = quizquestions[curentQuestion];
 var qTitle = document.createElement("h1");
@@ -39,10 +39,24 @@ qTitle.textContent= questions.question;
 questionsConainer.appendChild(qTitle);
 
 for (var i = 0; i < questions.answers.length; i++){
-
 var answersbuttons = document.createElement("button");
+answersbuttons.setAttribute('id','button'+i);
 answersbuttons.textContent = questions.answers[i];
 questionsConainer.appendChild(answersbuttons);
+    answersbuttons.addEventListener("click",function(){
+        if(questions.correctAnswer=== this.textContent){
+            console.log(true);
+            score++;
+        }
+
+
+    })
+
+
+answersbuttons.textContent = questions.answers[i];
+questionsConainer.appendChild(answersbuttons);
+
+// colect choice from click
 }
 curentQuestion ++;
 }
